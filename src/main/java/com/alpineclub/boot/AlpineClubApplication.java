3e7c5611ae4b609aa8136615124b1a6f6e4291dc
JiHyun, Jeong <39480072+dinb1242@users.noreply.github.com>
2022-06-09 20:32:07 +0900

    개발환경 생성

---
+++ src/main/java/com/alpineclub/boot/AlpineClubApplication.java
@@ -0,0 +1,13 @@
+package com.alpineclub.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpineClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlpineClubApplication.class, args);
+    }
+
+}
